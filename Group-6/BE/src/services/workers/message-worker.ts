14891a8daf2f3a8861ee55288fba478151a4959f
Aitor <aitor.benitez@colba.es>
2026-02-26 14:11:14 +0100

    Added event listener to queue

--- Group-6/BE/src/services/workers/message-worker.ts
+++ Group-6/BE/src/services/workers/message-worker.ts
@@ -1,18 +1,52 @@
+import axios, { AxiosRequestConfig } from 'axios';
 import { MessageData } from '../../models/messageData';
 import { queueService } from '../queue';
+import PQueue from 'p-queue';
+
+const workerQueue = new PQueue({ concurrency: 100 });
 
 export const initWorker = () => {
-    console.log("🚀 Worker listening...");
+    console.log("Worker listening...");
 
     queueService.on('messageAdded', (message: MessageData) => {
         console.log(`New message detected: ${message.id}`);
-        processMessage(message);
+        workerQueue.add(async () => {
+            await processMessage(message);
+        });
     });
 };
 
-async function processMessage(message: MessageData) {
+async function processMessage(message: MessageData, retryNumber: number = 0) {
     console.log(`Processing message:`, message.payload);
     
-    queueService.dequeue();
-    console.log(`Process finished: ${queueService.length}`);
+    const config: AxiosRequestConfig = {
+        //todo change this
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer TU_TOKEN_AQUI',
+            'X-Custom-Header': 'ValorPersonalizado'
+        },
+        timeout: 5000
+    };
+
+    try {
+        const response = await axios.post(message.url, message.payload, config);
+
+        console.log(`Set message succesfully. ${response.status} - ${JSON.stringify(response.data)}`);
+        queueService.dequeue();
+        //TODO - Update STATUS of the message stored on db
+
+        return response.data;
+    } catch (error: any) {
+        if (axios.isAxiosError(error)) {
+            console.error(`Error HTTP: ${error.response?.status} - ${error.message}`);
+        } else {
+            console.error(`Error:`, error);
+        }
+
+        if(retryNumber > 3)
+            throw error;
+
+        await processMessage(message, retryNumber++);
+    }
 }
