fd68be5b6260c79e6e981ab1d6087bc022486367
guivicj <guillem.vicente.juan@gmail.com>
2026-02-27 09:40:20 +0100

    schedule timer update

--- Group-6/BE/src/services/workers/scheduler.worker.ts
+++ Group-6/BE/src/services/workers/scheduler.worker.ts
@@ -10,5 +10,5 @@ export async function checkScheduledMessages() {
         if (message && message.schedule <= now) {
             processMessage(message, scheduledQueueService)
         }
-    }, 500000)
+    }, 60000)
 }
